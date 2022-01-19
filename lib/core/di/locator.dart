4842e7f43136703bceb0f29e273ee8c078c96474
omer.tajelsir <omer@blockchaincommodities.ae>
2022-01-19 12:46:20 +0400

    stash

--- lib/core/di/locator.dart
+++ lib/core/di/locator.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter_app/core/network/dio_request_manager.dart';
 import 'package:flutter_app/core/network/i_api_request_manager.dart';
 import 'package:flutter_app/data/btc/btc_api_client.dart';
@@ -7,13 +6,12 @@ import 'package:get_it/get_it.dart';
 
 GetIt locator = GetIt.instance;
 
-void setupLocator (){
-  
+void setupLocator() {
   locator.registerSingleton<IApiRequestManager>(DioRequestManager());
-  
-  locator.registerFactory<BTCApiClient>(() => BTCApiClient(requestManager: locator<IApiRequestManager>()));
 
-  locator.registerFactory<BTCRepository>(() => BTCRepository(btcApiClient: locator<BTCApiClient>()));
-  
-  
+  locator.registerFactory<BTCApiClient>(
+      () => BTCApiClient(requestManager: locator<IApiRequestManager>()));
+
+  locator.registerFactory<BTCRepository>(
+      () => BTCRepository(btcApiClient: locator<BTCApiClient>()));
 }
